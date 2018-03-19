62b582bfda46a483bc0d3bd821d4050716ba9f6a
Melissa Hill <mhill2040@gmail.com>
2018-03-19 12:55:15 -0600

    classroom app

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
+import 'semantic-ui-css/semantic.min.css';
+import { ThemeProvider } from 'styled-components';
+
+const theme = {
+  fg: 'white',
+  bg: 'black',
+  nav: 'purple',
+}
+
+ReactDOM.render(
+<ThemeProvider theme={theme}>
+  <App />
+</ThemeProvider>,
+  document.getElementById('root'));
+registerServiceWorker();
